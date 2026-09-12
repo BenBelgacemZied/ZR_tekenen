ca7d443123d6b75223360e51148cbef1b221a6a5
BenBelgacemZied <64262694+BenBelgacemZied@users.noreply.github.com>
2026-09-12 22:24:33 +0200

    Voeg app startcode toe

---
+++ App.xaml.cs
@@ -0,0 +1,10 @@
+namespace ZR_tekenen;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new NavigationPage(new MainPage());
+    }
+}
